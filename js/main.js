8db27b152f56170d6adcaf2a00162458a1f542c6
Taziev Rodion <morrowcj81@gmail.com>
2017-09-07 16:41:36 +0700

    Справочник строителя desktop без slider-a

---
+++ js/main.js
@@ -0,0 +1,22 @@
+
+$(document).ready(function () {
+    $(".js_scroll").on("click", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
