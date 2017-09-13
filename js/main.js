f58d45dc1c25f162809806b83c2a292eaa890771
Taziev Rodion <morrowcj81@gmail.com>
2017-09-14 03:24:51 +0700

    Справочник строителя Desktop + Адаптив final 5

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function () {
     $(".js_scroll").on("click", function (event) {
         event.preventDefault();
@@ -6,6 +5,41 @@ $(document).ready(function () {
             top = $(id).offset().top;
         $('body,html').animate({scrollTop: top}, 1500);
     });
+
+
+    $(".buttons__in-cart").first().click(function(e) {
+        var aForm = $(this).closest("#advertize-form");
+        var aDescription = aForm.find(".advertize__text-desciption");
+        var aPhone = aForm.find(".advertize__phone");
+        var introSelect1link = aForm.find('.intro-select1-link');
+        var introSelect1 = aForm.find('.intro-select1');
+
+        introSelect1.on('change', '', function (e) {
+            introSelect1link.removeClass("red");
+        });
+
+        aDescription.keyup(function () {
+            $(this).removeClass("red");
+        });
+
+        aPhone.keyup(function () {
+            $(this).removeClass("red");
+        });
+
+        if (aDescription.val() == 0) {
+            aDescription.addClass("red");
+        }
+
+        if (aPhone.val() == 0) {
+            aPhone.addClass("red");
+        }
+
+        if($('.intro-select1')[0].selectedIndex <= 0) {
+            introSelect1link.addClass("red");
+        }
+        e.preventDefault();
+    });
+
 });
 
 
