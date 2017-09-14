7d6ff174c92d61cb36ba232460ce16d7856853fb
Taziev Rodion <morrowcj81@gmail.com>
2017-09-15 01:08:18 +0700

    Cправочник строителя Bugs

--- js/main.js
+++ js/main.js
@@ -7,45 +7,120 @@ $(document).ready(function () {
     });
 
 
-    $(".buttons__in-cart").first().click(function(e) {
-        var aForm = $(this).closest("#advertize-form");
-        var aDescription = aForm.find(".advertize__text-desciption");
-        var aPhone = aForm.find(".advertize__phone");
-        var introSelect1link = aForm.find('.intro-select1-link');
-        var introSelect1 = aForm.find('.intro-select1');
+    var mass = [];
+    $('.banner-advertize').each(function(index) {
+        mass.push(this);
+    });
+
+    for (var i = 0; i < mass.length; i++) {
+
+        $(".buttons__in-cart").eq(i).bind( "click", function(e)  {
+
+            var advForm,
+                advDescription,
+                advPhone,
+                introSelect1link,
+                advNameorg,
+                introSelect1;
+
+            advForm = $(this).closest("#banner-advertize-form");
+            advDescription = advForm.find(".banner-advertize__text-desciption");
+            advNameorg = advForm.find(".banner-advertize__name-organization");
+            advPhone = advForm.find(".banner-advertize__phone");
+            introSelect1link = advForm.find('.intro-select1-link');
+            introSelect1 = advForm.find('.intro-select1');
+
+            if (advDescription.val() == 0) {
+                advDescription.removeClass("normal");
+                advDescription.addClass("red");
+            }
+
+            if (advPhone.val() == 0) {
+                advPhone.removeClass("normal");
+                advPhone.addClass("red");
+            }
+
+            if (advNameorg.val() == 0) {
+                advNameorg.removeClass("normal");
+                advNameorg.addClass("red");
+            }
+
+            if ($('.intro-select1')[0].selectedIndex <= 0) {
+                introSelect1link.removeClass("normal");
+                introSelect1link.addClass("red");
+            }
+
+            introSelect1.on('change', '', function (e) {
+                introSelect1link.removeClass("red");
+                introSelect1link.addClass("normal");
+            });
+
+            advDescription.keyup(function () {
+                $(this).removeClass("red");
+                $(this).addClass("normal");
+            });
+
+            advNameorg.keyup(function () {
+                $(this).removeClass("red");
+                $(this).addClass("normal");
+            });
+
+            advPhone.keyup(function () {
+                $(this).removeClass("red");
+                $(this).addClass("normal");
+            });
+
+            e.preventDefault();
+        });
+    }
+
+
+    $(".buttons__in-cart").first().bind( "click", function(e)  {
+
+        var advForm,
+            advDescription,
+            advPhone,
+            introSelect1link,
+            introSelect1;
+
+        advForm = $(this).closest("#advertize-form");
+        advDescription = advForm.find(".advertize__text-desciption");
+        advPhone = advForm.find(".advertize__phone");
+        introSelect1link = advForm.find('.intro-select1-link');
+        introSelect1 = advForm.find('.intro-select1');
+
+        if (advDescription.val() == 0) {
+            advDescription.removeClass("normal");
+            advDescription.addClass("red");
+        }
+
+        if (advPhone.val() == 0) {
+            advPhone.removeClass("normal");
+            advPhone.addClass("red");
+        }
+
+        if ($('.intro-select1')[0].selectedIndex <= 0) {
+            introSelect1link.removeClass("normal");
+            introSelect1link.addClass("red");
+        }
 
         introSelect1.on('change', '', function (e) {
             introSelect1link.removeClass("red");
             introSelect1link.addClass("normal");
         });
 
-        aDescription.keyup(function() {
+        advDescription.keyup(function () {
             $(this).removeClass("red");
             $(this).addClass("normal");
         });
 
-        aPhone.keyup(function() {
+        advPhone.keyup(function () {
             $(this).removeClass("red");
             $(this).addClass("normal");
         });
 
-        if (aDescription.val() == 0) {
-            aDescription.removeClass("normal");
-            aDescription.addClass("red");
-        }
-
-        if (aPhone.val() == 0) {
-            aPhone.removeClass("normal");
-            aPhone.addClass("red");
-        }
-
-        if($('.intro-select1')[0].selectedIndex <= 0) {
-            introSelect1link.removeClass("normal");
-            introSelect1link.addClass("red");
-        }
         e.preventDefault();
     });
-
 });
 
 
