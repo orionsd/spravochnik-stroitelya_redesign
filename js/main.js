6b520d5fcf43545fa7b7b737eaef62adc36d3d38
Taziev Rodion <morrowcj81@gmail.com>
2017-09-16 03:07:06 +0700

    Cправочник строителя Bug fixing

--- js/main.js
+++ js/main.js
@@ -6,14 +6,60 @@ $(document).ready(function () {
         $('body,html').animate({scrollTop: top}, 1500);
     });
 
+    $(".buttons__in-cart_text").first().bind( "click", function(e)  {
+
+        var advForm,
+            advDescription,
+            advPhone,
+            introSelect1link,
+            introSelect1;
+
+        advForm = $(this).closest("#advertize-form");
+        advDescription = advForm.find(".advertize__text-desciption");
+        advPhone = advForm.find(".advertize__phone");
+        introSelect1link = advForm.find('.intro-select1-link');
+        introSelect1 = advForm.find('.intro-select1');
+
+        if (advDescription.val() == 0) {
+            advDescription.removeClass("normal");
+            advDescription.addClass("red");
+        }
+
+        if (advPhone.val() == 0) {
+            advPhone.removeClass("normal");
+            advPhone.addClass("red");
+        }
+
+        if ($('.intro-select1')[0].selectedIndex <= 0) {
+            introSelect1link.removeClass("normal");
+            introSelect1link.addClass("red");
+        }
+
+        introSelect1.on('change', '', function (e) {
+            introSelect1link.removeClass("red");
+            introSelect1link.addClass("normal");
+        });
+
+        advDescription.keyup(function () {
+            $(this).removeClass("red");
+            $(this).addClass("normal");
+        });
+
+        advPhone.keyup(function () {
+            $(this).removeClass("red");
+            $(this).addClass("normal");
+        });
+
+        e.preventDefault();
+    });
+
 
     var mass = [];
     $('.banner-advertize').each(function(index) {
         mass.push(this);
     });
 
-    for (var i = 0; i < mass.length; i++) {
-
+    for (var i = 0; i < (mass.length); i++) {
         $(".buttons__in-cart").eq(i).bind( "click", function(e)  {
 
             var advForm,
@@ -23,7 +69,8 @@ $(document).ready(function () {
                 advNameorg,
                 introSelect1;
 
-            advForm = $(this).closest("#banner-advertize-form");
+            advForm = $(this).closest(".banner-advertize-form");
+            console.log(advForm)
             advDescription = advForm.find(".banner-advertize__text-desciption");
             advNameorg = advForm.find(".banner-advertize__name-organization");
             advPhone = advForm.find(".banner-advertize__phone");
@@ -75,52 +122,6 @@ $(document).ready(function () {
     }
 
 
-    $(".buttons__in-cart").first().bind( "click", function(e)  {
-
-        var advForm,
-            advDescription,
-            advPhone,
-            introSelect1link,
-            introSelect1;
-
-        advForm = $(this).closest("#advertize-form");
-        advDescription = advForm.find(".advertize__text-desciption");
-        advPhone = advForm.find(".advertize__phone");
-        introSelect1link = advForm.find('.intro-select1-link');
-        introSelect1 = advForm.find('.intro-select1');
-
-        if (advDescription.val() == 0) {
-            advDescription.removeClass("normal");
-            advDescription.addClass("red");
-        }
-
-        if (advPhone.val() == 0) {
-            advPhone.removeClass("normal");
-            advPhone.addClass("red");
-        }
-
-        if ($('.intro-select1')[0].selectedIndex <= 0) {
-            introSelect1link.removeClass("normal");
-            introSelect1link.addClass("red");
-        }
-
-        introSelect1.on('change', '', function (e) {
-            introSelect1link.removeClass("red");
-            introSelect1link.addClass("normal");
-        });
-
-        advDescription.keyup(function () {
-            $(this).removeClass("red");
-            $(this).addClass("normal");
-        });
-
-        advPhone.keyup(function () {
-            $(this).removeClass("red");
-            $(this).addClass("normal");
-        });
-
-        e.preventDefault();
-    });
 });
 
 
